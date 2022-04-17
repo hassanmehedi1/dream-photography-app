8e0ae55c4392b4453383de2009d54d24db299bcd
Mehedi Hassan <rehan51322@gmail.com>
2022-04-18 02:18:29 +0600

    github signIn added

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -10,11 +10,11 @@ const Banner = () => {
     setIndex(selectedIndex);
   };
   return (
-    <section className='container m-0'>
+    <section className="container mx-auto mt-4">
       <Carousel activeIndex={index} onSelect={handleSelect}>
         <Carousel.Item>
           <img className="d-block w-100" src={banner1} alt="First slide" />
-          <Carousel.Caption className="text-xl text-blue-500">
+          <Carousel.Caption className="text-lg text-blue-600 font-mono font-semibold">
             <h3>Wedding Photography</h3>
             <p>Premium Wedding Photo Session</p>
           </Carousel.Caption>
@@ -22,7 +22,7 @@ const Banner = () => {
         <Carousel.Item>
           <img className="d-block w-100" src={banner2} alt="Second slide" />
 
-          <Carousel.Caption className="text-xl text-blue-700">
+          <Carousel.Caption className="text-lg text-yellow-500 font-mono font-semibold">
             <h3>Portrait Photography</h3>
             <p>Excellent Portrait Session At Your Chosen Place</p>
           </Carousel.Caption>
@@ -30,7 +30,7 @@ const Banner = () => {
         <Carousel.Item>
           <img className="d-block w-100" src={banner3} alt="Third slide" />
 
-          <Carousel.Caption>
+          <Carousel.Caption className="text-lg font-mono font-semibold">
             <h3>Landscape Photography</h3>
             <p>Art Style Landscape Photography</p>
           </Carousel.Caption>
