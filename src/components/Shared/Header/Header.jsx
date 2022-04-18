8d4af2f2b6c17811b917e182faaf3d07a67924b1
Mehedi Hassan <rehan51322@gmail.com>
2022-04-18 16:42:56 +0600

    footer updated

--- src/components/Shared/Header/Header.jsx
+++ src/components/Shared/Header/Header.jsx
@@ -29,7 +29,7 @@ const Header = () => {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="home#services">Services</Nav.Link>
+              <Nav.Link  href="home#services">Services</Nav.Link>
               <Nav.Link href="home#reviews">Reviews</Nav.Link>
             </Nav>
             <Nav>
